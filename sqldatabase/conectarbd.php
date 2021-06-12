7cdbf993d2ebabd8e28f25eb511baa4952ebf010
unknown <lcueg00@estudiantes.unileon.es>
2021-06-12 22:48:20 +0200

    Actualizado funciones para recoger datos y actualizarlos

--- sqldatabase/conectarbd.php
+++ sqldatabase/conectarbd.php
@@ -1,10 +1,8 @@
 <?php
 
-include "./../debugops.php";
-
 function connectToDatabase() {
   $user = 'root'; //Esto es la url (una direccion ip)
-  $pass = 'mysqlroot';
+  $pass = '';
   $db = 'cromos';
   $mysqli = new mysqli('localhost', $user, $pass, $db) or die("No se puede conectar");
 
@@ -22,32 +20,109 @@ function closeConnection($mysqli) {
   $mysqli -> close();
 }
 
-//Metodo para imprimir datos de una consulta que se le pase pro parametros
-//Va haciendo echo para cada linea del resultado (formateada para mostrar todos sus datos)
+//Metodo para devolver todas las filas obtenidas de una consulta
 function recogerDatosConsulta($mysqli, $sql) {
   $result = $mysqli->query($sql);
-  //$numLineas = $result->num_rows;
-
-  while ($row = $result->fetch_assoc()) {
-    foreach($row as $key => $value){
-      echo $key . " " . $value .";";
-    }
-    echo "<br>";
-  }
+  return $result;
 }
 
-//Recoge el saldo del recogerSaldoUsuario
-//Lo muesta en formato: "Saldo: 0"
+define("COLUMNAIDSOCIO","IdSocio");
+define("COLUMNASALDOSOCIO","Saldo");
+
+function insertarUsuario($mysqli) {
+  $sql = "INSERT INTO socio (IdSocio, Saldo) VALUES (null,0)";
+  if ($mysqli->query($sql) === TRUE) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+  $sql = "SELECT * FROM socio ORDER BY IdSocio DESC";
+  $result = $mysqli->query($sql);
+  $row = $result->fetch_assoc();
+
+  return $row[COLUMNAIDSOCIO];
+}
+
+//Devuelve el saldo del usuario con el id insertado
 function recogerSaldoUsuario($mysqli,$idSocio) {
   $sql = "SELECT Saldo FROM socio WHERE IdSocio = $idSocio";
   $result = $mysqli->query($sql);
 
-  while ($row = $result->fetch_assoc()) {
-    foreach($row as $key => $value){
-      echo $key . " " . $value .";";
-    }
-    echo "<br>";
+  $row = $result->fetch_assoc();
+  return $row[COLUMNASALDOSOCIO];
+}
+
+//Recoge todos los usuarios de la base de datos
+function recogerSocios($mysqli) {
+  $sql = "SELECT * FROM socio";
+  $result = $mysqli->query($sql);
+  if($results->num_rows === 0) {
+    //no hay resultados
   }
+  return $result;
+}
+
+//Recoge todos los cromos de la base de datos
+function recogerCromos($mysqli) {
+  $sql = "SELECT * FROM cromo";
+  $result = $mysqli->query($sql);
+  if($results->num_rows === 0) {
+    //no hay resultados
+  }
+  return $result;
+}
+
+//Recoge todos los albunes de la base de datos
+function recogerAlbum($mysqli) {
+  $sql = "SELECT * FROM album";
+  $result = $mysqli->query($sql);
+  if($results->num_rows === 0) {
+    //no hay resultados
+  }
+  return $result;
+}
+
+
+//Recoge todos los cromos que tengan unidades disponibles de una coleccion
+function recogerCromosColeccionDisponibles($mysqli,$idColeccion) {
+  $sql = "SELECT * FROM cromo WHERE UnidadesDisponibles>0 AND IdColeccion=$idColeccion";
+  $result = $mysqli->query($sql);
+  if($results->num_rows === 0) {
+    //no hay resultados
+  }
+  return $result;
+}
+
+//Recoge el album de una coleccion si esta activa
+function recogerAlbumColeccionDisponibles($mysqli,$idColeccion) {
+  $sql = "SELECT * FROM album WHERE Estado='Activo'";
+  $result = $mysqli->query($sql);
+  if($results->num_rows === 0) {
+    //no hay resultados
+  }
+  return $result;
+}
+
+//Recoge todos los cromos que posee el usuario para una coleccion en concreto
+//Falta definir que ocurre si no tiene ningun cromo
+function recogerCromosColeccionSocio($mysqli,$idColeccion,$idSocio) {
+  $sql = "SELECT * FROM poseecromo WHERE IdColeccion=$idColeccion AND IdSocio=$idSocio";
+  $result = $mysqli->query($sql);
+  if($results->num_rows === 0) {
+    //no hay resultados
+  }
+  return $result;
+}
+
+//Recoge todos el album que posee el usuario para una coleccion en concreto
+//Falta definir que ocurre si no tiene ningun cromo
+function recogerAlbumColeccionSocio($mysqli,$idColeccion,$idSocio) {
+  $sql = "SELECT * FROM poseealbum WHERE IdColeccion=$idColeccion AND IdSocio=$idSocio";
+  $result = $mysqli->query($sql);
+  if($results->num_rows === 0) {
+    //no hay resultados
+  }
+  return $result;
 }
 
 function actualizarSaldoUsuario($mysqli,$idSocio,$nuevoSaldo) {
@@ -77,6 +152,19 @@ function actualizarCantidadCromoUsuario($mysqli,$idSocio,$idCromo,$nuevaCantidad
   }
 }
 
+function actualizarEstadoColeccion($mysqli,$idColeccion,$estado) {
+  if($estado==true) {
+    $sql = "UPDATE coleccion SET Estado='Activo' WHERE IdColeccion = $idColeccion";
+  } else {
+    $sql = "UPDATE coleccion SET Estado='Inactivo' WHERE IdColeccion = $idColeccion";
+  }
+  if ($mysqli->query($sql) === TRUE) {
+    echo "Actualizado Correctamente";
+  } else {
+    echo "Error al actualizar: " . $mysqli->error;
+  }
+}
+
 //Metodo para consultar los albunes (prueba)
 function recogerDatosAlbunes($mysqli) {
   $sql = "SELECT * FROM album";
@@ -93,19 +181,19 @@ function recogerDatosAlbunes($mysqli) {
 
 //Esto es una funcion de prueba. Recojo una imagen de la base
 //de datos y la muestro (probado con una unica entrada en la base de datos)
-function recogerImagenesCromos($mysqli) {
-  $sql = "SELECT * FROM cromo";
-  $result = $mysqli->query($sql);
-  $numLineas = $result->num_rows;
+//function recogerImagenesCromos($mysqli) {
+  //$sql = "SELECT * FROM cromo";
+  //$result = $mysqli->query($sql);
+  //$numLineas = $result->num_rows;
 
-  while ($row = $result->fetch_assoc()) {
-    $imageDataString = base64_encode($row['Imagen']);
-    $image = base64_decode($imageDataString);
-  }
-  header("content-type: image/jpeg");
+  //while ($row = $result->fetch_assoc()) {
+    //$imageDataString = base64_encode($row['Imagen']);
+    //$image = base64_decode($imageDataString);
+  //}
+  //header("content-type: image/jpeg");
 
-  echo $image;
-}
+//  echo $image;
+//}
 
 function recogerDatosCromos($mysqli) {
   $sql = "SELECT * FROM cromo";
@@ -121,12 +209,17 @@ $mysqli = connectToDatabase();
 //recogerDatosAlbunes($mysqli);
 //recogerDatosCromos($mysqli);
 
-//recogerSaldoUsuario($mysqli,2);
+//$saldo = recogerSaldoUsuario($mysqli,2);
+//echo $saldo;
 //recogerSaldoUsuario($mysqli,3);
-
+//recogerImagenesCromos($mysqli);
 //recogerSaldoUsuario($mysqli,2);
 //actualizarSaldoUsuario($mysqli,2,30);
 //actualizarCantidadCromoUsuario($mysqli,2,1,3);
+//actualizarEstadoColeccion($mysqli,1,false);
+//$id = insertarUsuario($mysqli);
+//echo $id;
+
 echo "Funciona";
 
 
