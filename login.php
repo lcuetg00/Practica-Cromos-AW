d9ebb927482a4aa5418c044ee561f9126247a01e
cesonico-ule <cbermf00@estudiantes.unileon.es>
2021-06-14 14:50:52 +0200

    Fusionados html y php de login y signup

--- login.php
+++ login.php
@@ -1,42 +1,72 @@
-<?php
-	define("CANNONICALROOTPATH", "./");
-	include "./sessionManagement.php";
+﻿<html>
 
-	if (isset($_SESSION["user"])) {
-		header("location: ./index.php");
-		exit();
-	}
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="style.css">
+    </head>
 
-	include_once "./cromos_header.php";
-?>
+    <body>
+      
+        <?php
+	        define("CANNONICALROOTPATH", "./");
+	        include "./sessionManagement.php";
 
-<section class="login-form">
-	<h2>Login</h2>
-	<div class="login-form-form">
-		<form action="dologin.php" method="post">
-			Nombre: <input type="text" name="user"></input></br>
-			Contraseña: <input type="text" name="password"></input></br>
-			<button type="submit" name="submit">Acceder</button></br>
-		</form>
-	</div>
-	<?php
-		if (isset($_GET["error"])) {
-			switch ($_GET["error"]) {
-				case "userNotSet";
-					echo "<p>El campo usuario está incompleto</p>";
-				break;
-				case "passwordNotSet";
-					echo "<p>El campo contraseña está incompleto</p>";
-				break;
-				case "userPasswordInvalid";
-					echo "<p>El usuario y/o la contraseña no existen en la base de datos</p>";
-				break;
-			}
-		}
-	 ?>
-</section>
+	        if (isset($_SESSION["user"])) {
+		        header("location: ./index.php");
+		        exit();
+	        }
 
+	        include_once "./cromos_header.php";
+        ?>
 
-<?php
-	include_once "./cromos_footer.php"
-?>
+        <section class="login-form">
+            <form action="dologin.php" method="post">
+              <div class="imgcontainer">
+                <br><img src="img/avatar.png" alt="Avatar" class="avatar">
+                <br><br><label><c>INICIAR SESION</c></label><br><br>
+              </div>
+
+              <div class="container">
+                <label for="uname"><b>Nombre de usuario: </b></label>
+                <input type="text" placeholder="Introduce tu nombre de usuario" name="user" required>
+                <br><br>
+
+                <label for="psw"><b>Contraseña: </b></label>
+                <input type="text" placeholder="Introduce tu contraseña" name="password" required>
+                <br><br>
+
+                <button type="submit" name ="submit"> Iniciar Sesión </button>
+                <!-- <label><input type="checkbox" checked="checked" name="remember"> Recordarme </label> --> 
+              </div>
+
+              <div class="container">
+                <button type="button" class="volverbtn"> Volver </button>
+                <span class="greentxt">¿No tienes cuenta?   <a href="#"> Registrarse </a></span>
+                <!-- <span class="psw">¿Has olvidado la <a href="#">contraseña</a>?</span> --> 
+              </div>
+              <?php
+		        if (isset($_GET["error"])) {
+			        switch ($_GET["error"]) {
+				        case "userNotSet";
+					        echo "<p>El campo usuario está incompleto</p>";
+				        break;
+				        case "passwordNotSet";
+					        echo "<p>El campo contraseña está incompleto</p>";
+				        break;
+				        case "userPasswordInvalid";
+					        echo "<p class='p'>El usuario y/o la contraseña no existen en la base de datos</p>";
+				        break;
+			        }
+		        }
+	          ?>
+
+            </form>
+        </section>
+
+        <?php
+	        include_once "./cromos_footer.php"
+        ?>
+    </body>
+
+</html>
+
--- signup.php
+++ signup.php
@@ -1,58 +1,80 @@
-<?php
-	define("CANNONICALROOTPATH", "./");
-	include "./sessionManagement.php";
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="style.css">
+</head>
 
-	if (isset($_SESSION["user"])) {
-		header("location: ./index.php");
-		exit();
-	}
+<body>
+    <?php
+	    define("CANNONICALROOTPATH", "./");
+	    include "./sessionManagement.php";
+
+	    if (isset($_SESSION["user"])) {
+		    header("location: ./index.php");
+		    exit();
+	    }
 	
-	include_once "./cromos_header.php";
-?>
+	    include_once "./cromos_header.php";
+    ?>
 
-<section class="signup-form">
-	<h2>Sign Up</h2>
-	<div class="signup-form-form">
-		<form action="dosignup.php" method="post">
-			Nombre: <input type="text" name="user"></input></br>
-			Contraseña: <input type="text" name="password"></input></br>
-			Contraseña (de nuevo): <input type="text" name="passwordrep"></input></br>
-			<button type="submit" name="submit">Registrar</button></br>
-		</form>
-	</div>
-	<?php
-		if (isset($_GET["error"])) {
-			switch ($_GET["error"]) {
-				case "userNotSet";
-					echo "<p>El campo usuario está incompleto</p>";
-				break;
-				case "passwordNotSet";
-					echo "<p>El campo contraseña está incompleto</p>";
-				break;
-				case "passwordDoesntMatch":
-					echo "<p>Las contraseñas no coinciden</p>";
+    <section class="signup-form">
+        <div class="signup-form-form">
+            <form action="dosignup.php" method="post">
+              <div class="imgcontainer">
+                <br><img src="img/avatar.png" alt="Avatar" class="avatar">
+                <br><br><label><c> REGISTRARSE </c></label><br><br>
+              </div>
+
+              <div class="container">
+                <label for="uname"><b>Nombre de usuario: </b></label>
+                <input type="text" placeholder="Introduce tu nombre de usuario" name="user" required>
+                <br><br>
+
+                <label for="psw"><b>Contrase&ntilde;a: </b></label>
+                <input type="text" placeholder="Introduce tu contrase&ntilde;a" name="password" required>
+                <br><br>
+
+                <label for="psw"><b>Vuelve a introducir la contrase&ntilde;a: </b></label>
+                <input type="text" placeholder="Vuelve a introducir la contrase&ntilde;a" name="passwordrep" required>
+                <br><br>
+
+                <button type="submit" name="submit"> Crear Cuenta </button>
+              </div>
+
+              <div class="container">
+                <button type="button" style="float: right;" class="volverbtn"> Volver </button>
+              </div>
+              <br><br><br>
+            </form>
+        </div>
+			<?php
+			if (isset($_GET["error"])) {
+				switch ($_GET["error"]) {
+					case "passwordDoesntMatch":
+						echo "<p>Las contrase&ntilde;as no coinciden</p>";
+						break;
+					case "userInvalid";
+						echo "<p>El campo usuario debe contener de 4 a 16 caracteres alfadecimales y debe empezar por una letra</p>";
 					break;
-				case "userInvalid";
-					echo "<p>El campo usuario debe contener de 4 a 16 caracteres alfadecimales y debe empezar por una letra</p>";
-				break;
-				case "passwordInvalid";
-					echo "<p>El campo contraseña debe contener de 6 a 16 caracteres alfadecimales, incluídos \"! ? . -\"</p>";
-				break;
-				case "passwordUnsafe";
-					echo "<p>La contraseña introducida no es lo suficientemente segura</p>";
-				break;
-				case "userAlreadyExists";
-					echo "<p>El usuario introducido ya existe</p>";
-				break;
-				case "";
-					echo "<p></p>";
-				break;
+					case "passwordInvalid";
+						echo "<p>El campo contrase&ntilde;a debe contener de 6 a 16 caracteres alfadecimales, inclu�dos \"! ? . -\"</p>";
+					break;
+					case "passwordUnsafe";
+						echo "<p>La contrase&ntilde;a introducida no es lo suficientemente segura</p>";
+					break;
+					case "userAlreadyExists";
+						echo "<p>El usuario introducido ya existe</p>";
+					break;
+					case "";
+						echo "<p></p>";
+					break;
+				}
 			}
-		}
-	 ?>
-</section>
+		 ?>
+    </section>
+    <?php
+	    include_once "cromos_footer.php"
+    ?> 
+</body>
 
-
-<?php
-	include_once "cromos_footer.php"
-?>
+</html>
